b210642b674e4989704491765e3db443063f06d7
hmjmf <zqx5449@gmail.com>
2018-02-28 22:28:36 +0800

    echo

--- echo/echo_service_tcp.cpp
+++ echo/echo_service_tcp.cpp
@@ -11,59 +11,99 @@
 
 
 #include <iostream>
+#include <glog/logging.h>
 #include <boost/asio.hpp>
 #include <boost/thread/thread.hpp>
 #include <boost/bind.hpp>
 #include <boost/date_time/posix_time/posix_time.hpp>
 
-class printer {
+
+using boost::asio::ip::tcp;
+
+class session: public std::enable_shared_from_this<session>{
 public:
-  printer(boost::asio::io_service& io)
-      : strand_(io),
-        timer1_(io, boost::posix_time::seconds(1)),
-        timer2_(io, boost::posix_time::seconds(1)),
-        count_(0){
-    //handler 使用 boost::asio::strand::wrap()  包装
-    timer1_.async_wait(strand_.wrap(boost::bind(&printer::print1, this)));
-    timer2_.async_wait(strand_.wrap(boost::bind(&printer::print2, this)));
-  }
+  session(tcp::socket socket):socket_(std::move(socket)){}
 
-  ~printer() {
-    std::cout << "Final count is " << count_ << std::endl;
-  }
-  void print1() {
-    if (count_ < 10) {
-      std::cout << "Timer 1: " << count_ << std::endl;
-      ++count_;
-      //handler 使用 boost::asio::strand::wrap()  包装
-      timer1_.expires_at(timer1_.expires_at() + boost::posix_time::seconds(1));
-      timer1_.async_wait(strand_.wrap(boost::bind(&printer::print1, this)));
-    }
-  }
-
-  void print2() {
-    if (count_ < 10) {
-      std::cout << "Timer 2: " << count_ << std::endl;
-      ++count_;
-      //handler 使用 boost::asio::strand::wrap()  包装
-      timer2_.expires_at(timer2_.expires_at() + boost::posix_time::seconds(5));
-      timer2_.async_wait(strand_.wrap(boost::bind(&printer::print2, this)));
-    }
+  void start(){
+    do_read();
   }
 
 private:
-  boost::asio::io_service::strand strand_;
-  boost::asio::deadline_timer timer1_;
-  boost::asio::deadline_timer timer2_;
-  int count_;
+  typedef boost::system::error_code error_code;
+
+  void do_read(){
+    auto self(shared_from_this());
+    socket_.async_read_some(boost::asio::buffer(data_, max_length),
+        [this, self](error_code errorCode, std::size_t len){
+          if(!errorCode){
+            do_write(len);
+          } else {
+            LOG(ERROR) << "do_read error : " << errorCode.value();
+          }
+        }
+    );
+  }
+  void do_write(std::size_t len){
+    auto self(shared_from_this());
+    boost::asio::async_write(socket_, boost::asio::buffer(data_, len),
+        [this, self](error_code errorCode, std::size_t /*len*/){
+          if(!errorCode){
+            do_read();
+          } else {
+            LOG(ERROR) << "do_write error : " << errorCode.value();
+          }
+        }
+    );
+  }
+
+  tcp::socket socket_;
+  enum { max_length = 2048 };
+  char data_[max_length];
 };
 
-int main()
-{
+class echo_service{
+public:
+  echo_service(boost::asio::io_context& context, short port):
+      acceptor_(context, tcp::endpoint(tcp::v4(), port)) {
+    do_accept();
+  }
+private:
+  typedef boost::system::error_code error_code;
+  void do_accept(){
+    acceptor_.async_accept(
+        [this](error_code errorCode, tcp::socket socket){
+          if(!errorCode){
+            std::make_shared<session>(std::move(socket))->start();
+          } else {
+            LOG(ERROR) << "do_accept error : " << errorCode.value();
+          }
+          do_accept();
+    });
+  }
+
+  tcp::acceptor acceptor_;
+};
+
+int main(int argc, char* argv[]) {
+  FLAGS_alsologtostderr = 1;
+  google::InitGoogleLogging(argv[0]);
+
+
+  try{
+    if(argc != 3){
+      LOG(ERROR) << "Usage echo_service_tcp <log_path> <port>";
+      return 1;
+    }
+    google::SetLogDestination(google::GLOG_INFO, argv[1]);
+
+    boost::asio::io_context context;
+    echo_service s(context, std::atoi(argv[2]));
+    context.run();
+
+  } catch (std::exception& e){
+    LOG(FATAL) <<"Exception: " << e.what();
+  }
   boost::asio::io_service io;
-  printer p(io);
-  boost::thread t(boost::bind(&boost::asio::io_service::run, &io));
-  io.run();
-  t.join();
+
   return 0;
 }
