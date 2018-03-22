4e42454a46079f215f0e0c7c340a603b8d6e7ce7
hmjmf <zqx5449@gmail.com>
2018-03-22 10:53:08 +0800

    post

--- run_all_service.sh
+++ run_all_service.sh
@@ -1,6 +1,8 @@
+#!/usr/bin/env bash
 mkdir -p build
 cd build
 cmake ..
 make
 
 nohup ./echo/echo_service_tcp ./echo/echolog 2000 > echolog.txt &
+nohup ./post/post_service 127.0.0.1 2001 > post.txt &
